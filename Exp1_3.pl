7cfed465308227ef2bafbf7058ef2b357cc76926
Mr.Vipul <vipulupadhyay563@gmail.com>
2022-09-28 18:33:43 +0530

    initial commit

---
+++ Exp1_3.pl
@@ -0,0 +1,12 @@
+likes(mary,flowers).
+likes(bob,wine).
+likes(charlie,flowers).
+likes(wini,oranges).
+likes(smith,flowers).
+likes(john,X):-likes(X,flowers).
+like(ram,apple).
+taller(ram,mohan).
+name(vipul).
+fruit(apple).
+fruit(orange).
+male(ram).
